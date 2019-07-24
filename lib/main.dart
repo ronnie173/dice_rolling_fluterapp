97da94f97903e3afc90a8e96ffc3d78f6ee7226b
Jerome Raymond <ronnie173@gmail.com>
2019-07-23 21:37:44 -0500

    Added Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,18 +15,29 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
+
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
-              child: Image.asset('images/dice1.png'),
               onPressed: () {
-                print('Clicked me');
+                setState(() {
+                  leftDiceNumber = 6;
+                });
+
               },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+
             ),
           ),
           Expanded(
@@ -38,5 +49,7 @@ class DicePage extends StatelessWidget {
         ],
       ),
     );
-  }
+}
+
+
 }
